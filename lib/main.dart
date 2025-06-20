a68b90c6a2535a16343ecc5ca40d2c25c7f7f041
Heet Limbani <heetlimbani9999@gmail.com>
2025-06-20 10:39:49 +0530

    Fire Base Update

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sportzy/Home/Tabs/homeScreen.dart';
+import 'package:sportzy/firebase_options.dart';
 import 'Signup/Login/loginScreen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setPreferredOrientations(
     [
       DeviceOrientation.portraitUp,
